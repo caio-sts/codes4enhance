5790b85758f180bf34f60e176654296cf7a8bd46
Caio <67929382+caio-sts@users.noreply.github.com>
2022-08-13 06:35:28 -0300

    Delete denoisebuiltin.py

--- codesEnhance/denoisebuiltin.py
+++
@@ -1,8 +0,0 @@
-def denoTV(img1):
-  return denoise_tv_chambolle(img1, weight=0.1, multichannel=True)
-
-def denoBilat(img1):
-  return denoise_bilateral(img1, multichannel=True, sigma_color=0.05, sigma_spatial=15)
-
-def denoWavelet(img1):
-  return denoise_wavelet(img1, multichannel=True, mode="soft", rescale_sigma=True)
