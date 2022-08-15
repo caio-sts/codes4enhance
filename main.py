ab847a57165fe2a23e90358f29790151a80157b4
Caio <67929382+caio-sts@users.noreply.github.com>
2022-08-14 21:19:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from inFolder import enhance
+from inFolder import enhance, geometricImageAugmentation
 
 def main():
     parser = argparse.ArgumentParser(description='Enhance images from a directory.')
@@ -9,17 +9,30 @@ def main():
     parser.add_argument(
         '-s', '--save', required=True, type=str, help='PATH to save')
     parser.add_argument(
-        '-n', '--methodname', required=True, type=str, help='method of enhancement')
-    args = parser.parse_args()
+        '-n', '--methodname', nargs="?", type=str, help='method of enhancement')
+    parser.add_argument(
+        '-g', '--geometrics', nargs="?", type=bool, help='geometric transformations for data augmentation', default= False)
+    parser.add_argument(
+        '-x', '--numberGenerat', nargs="?", type=int, help='number of transformations for data augmentation')
+    parser.add_argument(
+        '-sh', '--shear', nargs="?", type=int, help='shear angle for data augmentation')
+    parser.add_argument(
+        '-r', '--rotation', nargs="?", type=int, help='rotation angle for data augmentation')
+    parser.add_argument(
+        '-tx', '--translatx', nargs="?", type=int, help='scale factor for horizontal translation for data augmentation')
+    parser.add_argument(
+        '-ty', '--translaty', nargs="?", type=int, help='scale factor for vertical translation for data augmentation')
     
+    args = parser.parse_args()
 
-    pathOrigin = args.origin
 
-    path2Save = args.save
+    if args.methodname != None:
+        enhance(args.origin, args.save, args.methodname)
+    
+    elif args.geometrics == True:
+        geometricImageAugmentation(args.origin, args.save, args.numberGenerat, 
+                                    args.shear, args.rotation, args.translatx, args.translaty)
 
-    method = args.methodname
-
-    enhance(pathOrigin, path2Save, method)
 
 
 if __name__ == "__main__":
